595d6b54d2066b4a3639754fd1dd0184d9a4b16a
Timo Hirsch-Hoffmann <timohiho@yahoo.com>
2019-11-22 20:44:11 +0100

    Reformat Code

--- static_embed.js
+++ static_embed.js
@@ -5,74 +5,74 @@
 
 const Discord = require('discord.js');
 
-    const TrialModColor = "#c8d5ee";
-    const wrongVerifyColor = "#ff0000";
-    const NotAllowedCharacterColor = "#ff0000";
-    const NoDiscordInviteColor = "#ff0000";
-    const WelcomeMessageColor = "#00ff00";
+const TrialModColor = "#c8d5ee";
+const wrongVerifyColor = "#ff0000";
+const NotAllowedCharacterColor = "#ff0000";
+const NoDiscordInviteColor = "#ff0000";
+const WelcomeMessageColor = "#00ff00";
 
-    function TrialModAccept(accepter, trialmod, guild) {
-        return new Discord.RichEmbed()
-            .setColor(this.TrialModColor)
-            .setAuthor(accepter.username, accepter.avatarURL)
-            .setTitle(`Hi, your mod application in ${guild.name} has been accepted!`)
-            .setTimestamp()
-            .setFooter(`For ${trialmod.user.username}, made by VukAnd12 and Gravity Assist`);
-    }
+function TrialModAccept(accepter, trialmod, guild) {
+    return new Discord.RichEmbed()
+        .setColor(TrialModColor)
+        .setAuthor(accepter.username, accepter.avatarURL)
+        .setTitle(`Hi, your mod application in ${guild.name} has been accepted!`)
+        .setTimestamp()
+        .setFooter(`For ${trialmod.user.username}, made by VukAnd12 and Gravity Assist`);
+}
 
-    function TrialModAddWarn(trialmod) {
-        return new Discord.RichEmbed() // Creates the embed thats returned to the person warning if its sent.
-            .setColor(this.TrialModColor)
-            .setTitle(`${trialmod.user.tag} has been given Trial Mod!`)
-            .setTimestamp();
-    }
+function TrialModAddWarn(trialmod) {
+    return new Discord.RichEmbed() // Creates the embed thats returned to the person warning if its sent.
+        .setColor(TrialModColor)
+        .setTitle(`${trialmod.user.tag} has been given Trial Mod!`)
+        .setTimestamp();
+}
 
-    function WrongVerifyMessage(member) {
-        return new Discord.RichEmbed()
-            .setColor(this.wrongVerifyColor)
-            .setTitle('You do not have permission to access discord://AAA')
-            .setAuthor('Verification System', 'https://b.thumbs.redditmedia.com/OIDktcKCqI8n4CnTj2SNZAQtXjBWxo9Qah6ku96YsME.png')
-            .setDescription('That\'s the wrong password. Please try again.')
-            .setTimestamp()
-            .setFooter(`For ${member.user.username}, made by VukAnd12 and Gravity Assist`)
-            .setImage("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/facebook/11/cross-mark_274c.png");
-    }
+function WrongVerifyMessage(member) {
+    return new Discord.RichEmbed()
+        .setColor(wrongVerifyColor)
+        .setTitle('You do not have permission to access discord://AAA')
+        .setAuthor('Verification System', 'https://b.thumbs.redditmedia.com/OIDktcKCqI8n4CnTj2SNZAQtXjBWxo9Qah6ku96YsME.png')
+        .setDescription('That\'s the wrong password. Please try again.')
+        .setTimestamp()
+        .setFooter(`For ${member.user.username}, made by VukAnd12 and Gravity Assist`)
+        .setImage("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/facebook/11/cross-mark_274c.png");
+}
 
-    function NotAllowedCharacter(member, character) {
-        return new Discord.RichEmbed()
-            .setColor(this.NotAllowedCharacterColor)
-            .setTitle('Really funny.')
-            .setDescription(`${character} is not allowed here, for obvious reasons.`)
-            .setImage('https://media.tenor.com/images/002ad9767dabb97d0dfcc0300ad95da7/tenor.gif')
-            .setTimestamp()
-            .setFooter(`For ${member.user.username}`);
-    }
+function NotAllowedCharacter(member, character) {
+    return new Discord.RichEmbed()
+        .setColor(NotAllowedCharacterColor)
+        .setTitle('Really funny.')
+        .setDescription(`${character} is not allowed here, for obvious reasons.`)
+        .setImage('https://media.tenor.com/images/002ad9767dabb97d0dfcc0300ad95da7/tenor.gif')
+        .setTimestamp()
+        .setFooter(`For ${member.user.username}`);
+}
 
-    function NoDiscordInvite(member) {
-        return new Discord.RichEmbed()
-            .setColor(this.NoDiscordInviteColor)
-            .setTitle('Rule 6!')
-            .setDescription('Per rule 6, it is not allowed to send invites to other Discord servers.')
-            .setImage('https://steamuserimages-a.akamaihd.net/ugc/961973556167374789/672A76928C54C3E57E081E0EB9E9A752B18B1778/')
-            .setTimestamp()
-            .setFooter(`For ${member.user.username}`);
-    }
+function NoDiscordInvite(member) {
+    return new Discord.RichEmbed()
+        .setColor(NoDiscordInviteColor)
+        .setTitle('Rule 6!')
+        .setDescription('Per rule 6, it is not allowed to send invites to other Discord servers.')
+        .setImage('https://steamuserimages-a.akamaihd.net/ugc/961973556167374789/672A76928C54C3E57E081E0EB9E9A752B18B1778/')
+        .setTimestamp()
+        .setFooter(`For ${member.user.username}`);
+}
 
-    function WelcomeMessage() {
-        return new Discord.RichEmbed()
-            .setColor(this.WelcomeMessageColor)
-            .setTitle('Welcome to the server!')
-            .setAuthor('Verification System', 'https://b.thumbs.redditmedia.com/OIDktcKCqI8n4CnTj2SNZAQtXjBWxo9Qah6ku96YsME.png')
-            .setDescription('To chat, you need to verify you\'ve read the rules. You can do so at <#638647989937897472>.\nAnd of course, AAAAAAAAAAAAAAA')
-            .setTimestamp()
-            .setFooter(`Made by VukAnd12`);
-    }
+function WelcomeMessage() {
+    return new Discord.RichEmbed()
+        .setColor(WelcomeMessageColor)
+        .setTitle('Welcome to the server!')
+        .setAuthor('Verification System', 'https://b.thumbs.redditmedia.com/OIDktcKCqI8n4CnTj2SNZAQtXjBWxo9Qah6ku96YsME.png')
+        .setDescription('To chat, you need to verify you\'ve read the rules. You can do so at <#638647989937897472>.\nAnd of course, AAAAAAAAAAAAAAA')
+        .setTimestamp()
+        .setFooter(`Made by VukAnd12`);
+}
 
-    module.exports = {
-        NoDiscordInvite,
-        WelcomeMessage,
-        NotAllowedCharacter,
-        WrongVerifyMessage,
-        TrialModAddWarn,
-        TrialModAccept
-      };
+module.exports = {
+    NoDiscordInvite,
+    WelcomeMessage,
+    NotAllowedCharacter,
+    WrongVerifyMessage,
+    TrialModAddWarn,
+    TrialModAccept
+};
--- static_functions.js
+++ static_functions.js
@@ -4,64 +4,68 @@
  */
 
 
-    function has_user_role(user, role_name) {
-        return user.roles.find(r => r.name === role_name);
+function has_user_role(user, role_name) {
+    return user.roles.find(r => r.name === role_name);
+}
+
+function get_role(guild, role_name) {
+    return guild.roles.find(r => r.name === role_name);
+}
+
+async function temp_message(content, channel, lifetime) {
+    channel.send(content).then(msg => {
+        msg.delete(lifetime)
+    });
+    return 9;
+}
+
+/**
+ * @return {boolean}
+ */
+function ContainsNoGuildDiscordInvite(content, guild) {
+    let search_text = content.toLowerCase();
+    let contains_no_guild_invite = false;
+    let n = 0;
+
+    const invites = guild.fetchInvites();
+    const guild_invites = [];
+
+    let message_invite = "";
+
+    for (const invite_code in invites) {
+        guild_invites.push(("discord.gg/" + invite_code).toLowerCase());
+        guild_invites.push(("discordapp.com/invite/" + invite_code).toLowerCase());
     }
 
-    function get_role(guild, role_name) {
-        return guild.roles.find(r => r.name === role_name);
-    }
+    while (search_text.includes("discord.gg/") && !contains_no_guild_invite) {
+        n = search_text.search("discord.gg/");
 
-    async function temp_message(content, channel, lifetime) {
-        channel.send(content).then(msg => {
-            msg.delete(lifetime)
-        });
-        return 9;
-    }
-
-    function ContainsNoGuildDiscordInvite(content, guild){
-        let search_text = content.toLowerCase();
-        let contains_no_guild_invite = true;
-        let n = 0;
-
-        const invites = guild.fetchInvites();
-        const guild_invites = [];
-
-        let message_invite = "";
-
-        for (const invite_code in invites) {
-            guild_invites.push(("discord.gg/" + invite_code).toLowerCase());
-            guild_invites.push(("discordapp.com/invite/" + invite_code).toLowerCase());
+        message_invite = search_text.substring(n, search_text.substring(n).search(" "));
+        if (!guild_invites.includes(message_invite)) {
+            contains_no_guild_invite = true;
         }
-
-        while (search_text.includes("discord.gg/") && contains_no_guild_invite) {
-            n = search_text.search("discord.gg/");
-
-            message_invite = search_text.substring(n, search_text.substring(n).search(" "));
-            if (!guild_invites.includes(message_invite)) {
-                contains_no_guild_invite = false;
-            }
-            search_text = search_text.substring(n + message_invite.length);
-        }
-
-        search_text = content.toLowerCase();
-
-        while (search_text.includes("discordapp.com/invite/") && contains_no_guild_invite) {
-            n = search_text.search("discordapp.com/invite/");
-
-            message_invite = search_text.substring(n, search_text.substring(n).search(" "));
-            if (!guild_invites.includes(message_invite)) {
-                contains_no_guild_invite = false;
-            }
-            search_text = search_text.substring(n + message_invite.length);
-        }
-
-        return contains_no_guild_invite;
+        search_text = search_text.substring(n + message_invite.length);
     }
 
-    module.exports = {
-        has_user_role,
-        get_role,
-        ContainsNoGuildDiscordInvite
-      };
+    search_text = content.toLowerCase();
+
+    while (search_text.includes("discordapp.com/invite/") && !contains_no_guild_invite) {
+        n = search_text.search("discordapp.com/invite/");
+
+        message_invite = search_text.substring(n, search_text.substring(n).search(" "));
+        if (!guild_invites.includes(message_invite)) {
+            contains_no_guild_invite = true;
+        }
+        search_text = search_text.substring(n + message_invite.length);
+    }
+
+    return contains_no_guild_invite;
+}
+
+module.exports = {
+    has_user_role,
+    get_role,
+    ContainsNoGuildDiscordInvite,
+    temp_message
+};
 
