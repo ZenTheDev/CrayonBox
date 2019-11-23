93f618aca74842036e0bd6861497bf5678b6a8ca
Vuk <46850780+VukAnd@users.noreply.github.com>
2019-11-23 13:53:02 +0100

    Includes, not contains

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ client.on('message', (message) => {
                 }
             } else if (channel.id === "560892008840036372") {
                 const exclude = content.replace(":crayonboxa:", ""); // Replace crayonboxa emoji
-                if (exclude.toLowerCase().contains("b")) {
+                if (exclude.toLowerCase().includes("b")) {
                     message.delete();
                     static_functions.temp_message(static_embed.NotAllowedCharacter(member, "B"), channel, 5000);
                 }
