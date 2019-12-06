ab83998a4038e4ec643f117116563f842b78773a
Timo Hirsch-Hoffmann <34097483+Timohiho@users.noreply.github.com>
2019-12-06 15:50:36 +0100

    invite fixer

--- bot.js
+++ bot.js
@@ -31,25 +31,112 @@ const modules = {
     3: bot_modules.banned_characters,
 };
 
+async function dev() {
 
+    const anarchy = client.guilds.get("652458385161322496");
+    var orginal_invite = jsonData["652458385161322496"]["invite"];
 
-client.once('ready', () => {
+    var orginal_exist = false;
+    var invites = await anarchy.fetchInvites();
+    var counter = 0;
+
+    if (invites.size === 0) {
+        counter = invites.size;
+    } else {
+
+        invites.forEach(invite => {
+            if (counter !== invites.size) {
+                const invite_code = invite.code;
+                if (invite_code === orginal_invite) {
+                    orginal_exist = true;
+                    counter = invites.size;
+                } else {
+                    counter += 1;
+                }
+            }
+        });
+    }
+
+    while (counter !== invites.size) {} // Wait until foreach is finish
+
+    if (!orginal_exist) {
+        let invite_channel = null;
+        let channel_counter = 0;
+        anarchy.channels.forEach(channel => {
+            if (channel_counter !== anarchy.channels.size) {
+                if (channel.type === "text") {
+                    invite_channel = channel;
+                    channel_counter = anarchy.channels.size
+                } else {
+                    channel_counter += 1;
+
+                }
+            }
+        });
+
+        while (channel_counter !== anarchy.channels.size) {
+        } // Wait until foreach is finish
+
+        if (invite_channel === null) {
+            invite_channel = await anarchy.createChannel("General", "text", false, "no text channel");
+        }
+        let newInvite = await invite_channel.createInvite({
+            maxUses: false, // After one use it will be void
+            unique: true, // That tells the bot not to use an existing invite so that this will be unique
+            maxAge: 0 // By default invites last 24 hours. If you want to change that, modify this (0 = lasts forever, time in seconds)
+        });
+
+        console.log("https://discord.gg/" + newInvite.code);
+
+        const Aserver = client.guilds.get("542073176729976842");
+        const announcements_channel = Aserver.channels.get("596308674876538880");
+
+        announcements_channel.send("https://discord.gg/" + newInvite.code);
+
+        jsonData[anarchy.id.toString()]["invite"] = newInvite.code;
+
+        fs.writeFileSync("./data.json", JSON.stringify(jsonData, null, 2));
+    } else {
+        // EVERYTHING OK
+    }
+}
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+client.once('ready', async () => {
     console.log('crayonbox-assist has started up successfully.');
+    while (1 === 1) {
+        dev();
+        await sleep(10000);
+    }
+});
+
+client.on('guildUpdate', (oldGuild, newGuild) => {
+    oldGuild.fetchInvites()
+        .then(invites => console.log(`Fetched ${invites.size} invites`))
+        .catch(console.error);
+    newGuild.fetchInvites()
+        .then(invites => console.log(`Fetched ${invites.size} invites`))
+        .catch(console.error);
 });
 
 client.on('message', (message) => {
 
+    /*if (message.content.toLowerCase() === "test") {
+        dev();
+    }*/
     let old_data = JSON.stringify(jsonData, null, 2);
-    
+
     const member = message.member;
     const channel = message.channel;
     const content = message.content;
-    
+
     const channelID = message.channel.id.toString();
 
     let guildID;
     let guild;
-    let prefix = jsonData["baseprefix"];
+    let prefix = "c!";
 
     if (channel.type !== "dm") {
         guild = message.guild;
@@ -68,7 +155,7 @@ client.on('message', (message) => {
 
             const args = message.content.slice(prefix.length).split(/ +/);
             const command = args.shift().toLowerCase();
-            
+
             if (channel.type !== "dm") {
                 if (jsonData[guildID]["commands"].hasOwnProperty(command)) {
                     jsonActions[jsonData[guildID]["commands"][command]["action"].toString()](message, client);
@@ -76,7 +163,7 @@ client.on('message', (message) => {
                     channel.send("Unknow command").then();
                 }
             }
-            
+
         } else {
 
             // solve everything over JSON Configuration
